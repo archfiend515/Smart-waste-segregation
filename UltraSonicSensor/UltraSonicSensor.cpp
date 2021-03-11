e4d461cc465175a455baf806c38c7c54373d7f5d
Aswin Subramanian Manickavasagam <2596691M@student.gla.ac.uk>
2021-03-11 19:51:46 +0000

    Stage one of UltraSonic Sensor

---
+++ UltraSonicSensor/UltraSonicSensor.cpp
@@ -0,0 +1,10 @@
+// UltraSonicSensor.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Initialization\n";
+}
+

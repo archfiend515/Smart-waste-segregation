0272101628173a5429e124ba66096c3a9825473b
Aswin Subramanian Manickavasagam <2596691M@student.gla.ac.uk>
2021-03-22 18:45:15 +0000

    Checked port and I2C device address

--- UltraSonicSensor/UltraSonicSensor.cpp
+++ UltraSonicSensor/UltraSonicSensor.cpp
@@ -1,10 +1,24 @@
-// UltraSonicSensor.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
-
+#include<stdio.h>
+#include<iostream>
+#include<linux/i2c-dev.h>
+#include<i2c/smbus.h>
+#include<unistd.h>
+using namespace std;
 int main()
 {
-    std::cout << "Initialization\n";
+	int file;
+	string port = "/dev/i2c-1";
+	file = open(port, O_RDWR);
+	if (file < 0)
+	{
+		printf("failed to open port \n");
+		exit(1);
+	}
+	int addr = 0x70;
+	if (ioctl(file, I2C_SLAVE, addr) < 0)
+	{
+		printf("unable to access device \n");
+		exit(1);
+	}
+	char buff[10];
 }
-

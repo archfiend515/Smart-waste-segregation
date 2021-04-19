f4c5011406f0f958ae7e96e43d888c4742f63492
Aswin Subramanian Manickavasagam <2596691M@student.gla.ac.uk>
2021-04-19 12:41:06 +0100

    test case 1

--- Lock and Led/electromagnet and led indicator.cpp
+++ Lock and Led/electromagnet and led indicator.cpp
@@ -1,10 +1,13 @@
 #include<wiringpi.h>
 int main()
 {
+	int distance;
 	int led = 4;
 	int lock = 26;
 	wiringPiSetup();
 	pinMode(led, OUTPUT);
+	pinMode(lock, OUTPUT);
+	distance = 10;
 	if (distance > 1)
 	{
 		digitalWrite(led, HIGH);
